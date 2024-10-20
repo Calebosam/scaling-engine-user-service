99d703f76d60fe9775db5e7e6fa31d28d256d01d
calebosam <caleb.osam@amalitech.com>
2024-10-20 01:16:58 +0000

    fix: resolve CORS issue

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PrismaClient } = require('@prisma/client');
 const dotenv = require('dotenv')
+const cors = require("cors")
 dotenv.config()
+
 
 const app = express();
 const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(bodyParser.json());
 
 // Create a new user
